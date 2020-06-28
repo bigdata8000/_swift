b8c66527f89480a9931277ed7466e85c7f6ea1b8
Henry Phu-Henh Lam <50170584+henrymmmm@users.noreply.github.com>
2020-06-28 01:17:57 +0100

    Add files via upload

---
+++ I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by henry on 28/06/2020.
+//  Copyright © 2020 henry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
